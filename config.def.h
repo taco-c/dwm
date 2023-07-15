0efe1d4c80a4fe1ce74d30abaf494815f1e22945
Rikard Karlsen <rk@cassettian.space>
2023-07-15 15:06:37 +0200

    fix: keymaps

--- config.def.h
+++ config.def.h
@@ -62,9 +62,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]     = { "st", NULL };
-static const char *surfcmd[]     = { "surf-open.sh", "https://searx.gnu.style", NULL };
+//static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[]    = { "rofi", "-show", "run", NULL };
+static const char *termcmd[]     = { "alacritty", NULL };
+static const char *surfcmd[]     = { "surf-open.sh", "https://duckduckgo.com/", NULL };
 static const char *suspendcmd[]  = { "systemctl", "suspend", NULL };
 
 /* volume commands */
